24f9a5c65f86ba013f6f6475ce95e3d54ac6c4a6
RogueDarkJedi <roguedarkjedi@users.noreply.github.com>
2020-03-01 14:07:09 -0800

    Add files via upload

---
+++ dogcamsocket.py
@@ -0,0 +1,88 @@
+import websocket
+import threading
+import time
+import json
+
+class DogCamSocket():
+  __socket: None
+  __thread: None
+  URL: ""
+  Processing: False
+  
+  def __init__(self, dest):
+    self.URL = dest
+    self.__socket = websocket.WebSocket()
+    
+  def __del__(self):
+    self.Disconnect()
+    
+  def Connect(self):
+    try:
+      self.__socket.connect(self.URL)
+      print("Socket connected!")
+      self.__OnConnected()
+    except KeyboardInterrupt:
+      print("Interrupted!")
+      raise
+    except:
+      print("Failed to connect!")
+      
+  def Disconnect(self):
+    self.Processing = False
+    
+    if self.__socket:
+      self.__socket.close()
+    
+    if self.__thread:
+      self.__thread.join()
+  
+  def SendPosition(self, direction):
+    if self.Processing is False or self.__socket is None:
+      print("Websocket is not connected!")
+      return
+
+    lowDir = direction.lower()
+    angleDir = 10.0
+    JsonMessage = {
+      "action": "moverel",
+      "source": "dogcamai"
+    }
+    
+    if lowDir is "left" or lowDir is "right":
+      JsonMessage.update(servo="pan")
+      if lowDir is "left":
+        angleDir *= -1
+    else:
+      JsonMessage.update(servo="tilt")
+      if lowDir is "top":
+        angleDir *= -1      
+    JsonMessage.update(angle=angleDir)
+    
+    print("Sending new postion message")
+    self.__socket.send(json.dumps(JsonMessage))
+  
+  def __OnConnected(self):
+    self.Processing = True
+    if self.__thread is None:
+      self.__thread = threading.Thread(target=self.__MessageThread, daemon=True, name="WSThread")
+      self.__thread.start()
+  
+  def __MessageThread(self):
+    while self.Processing:
+      try:
+        Message = self.__socket.recv()
+        if not Message:
+          time.sleep(2)
+          continue
+          
+        print(f"Got message: {Message}")
+
+      except websocket.WebSocketConnectionClosedException:
+        print("Socket has been disconnected!")
+        # self.Connect()
+        break
+      except KeyboardInterrupt:
+        break
+
+      time.sleep(2)
+  
--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 from dogcamstreamer import DogCamStreamer
 from dogcamai import DogCamAI
-import queue
-import time
+from dogcamsocket import DogCamSocket
 
 print("Starting streamer")
 dcs = DogCamStreamer("rtmp://192.168.50.4/camera/g")
+dcso = DogCamSocket("ws://192.168.50.169:5867")
 dcai = DogCamAI()
+
+print("Starting socket connection")
+dcso.Connect()
 
 print("Loading feed")
 if dcs.Start():
@@ -19,6 +22,8 @@ if dcs.Start():
     while not dcai.commandQueue.empty():
       command = dcai.commandQueue.get_nowait()
       print(f"Got command: {command}")
+      dcso.SendPosition(command)
       
 dcs.Stop()
 dcai.Stop()
+dcso.Disconnect()
