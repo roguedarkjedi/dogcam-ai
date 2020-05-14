80394da05b283fd6e9653281aa006f8f8c69e737
RogueDarkJedi <roguedarkjedi@users.noreply.github.com>
2020-05-14 15:26:39 -0700

    Fix whitespacing issues Codacy brought up

--- main.py
+++ main.py
@@ -4,17 +4,18 @@ from dogcamsocket import DogCamSocket
 from dogcamconfig import DogCamConfig
 
 print("Starting classes")
+
 dcc = DogCamConfig()
-dcs = DogCamStreamer(dcc.StreamingURL, 
-  timeBetweenCaptures=dcc.StreamingCaptureRate, 
-  disconnectionTimeout=dcc.StreamingTimeout, 
+dcs = DogCamStreamer(dcc.StreamingURL,
+  timeBetweenCaptures=dcc.StreamingCaptureRate,
+  disconnectionTimeout=dcc.StreamingTimeout,
   frameBufferSize=dcc.StreamingFrameBufferSize,
   videoFPS=dcc.StreamingFPS)
 
 dcso = DogCamSocket(dcc.CommandsAddress, MaxTimeout=dcc.CommandsTimeout)
 
-dcai = DogCamAI(boundsSize=dcc.AIBoundsSize, 
-  minimumConfidence=dcc.AIMinimumConfidence, 
+dcai = DogCamAI(boundsSize=dcc.AIBoundsSize,
+  minimumConfidence=dcc.AIMinimumConfidence,
   displayOut=dcc.AIDisplayVision,
   detectionID=dcc.AIDetectID,
   fpsSync=dcc.StreamingFPS)
@@ -26,11 +27,11 @@ if dcs.Start():
   dcs.Resize(newWidth=300, newHeight=300)
   dcai.SetDimensions(dcs.resWidth, dcs.resHeight)
   dcai.Start()
-  
+
   print("Starting main loop")
   while dcs.Running():
     dcai.PushImage(dcs.Read())
-    
+
     while dcai.commandQueue.empty() is False:
       command = dcai.commandQueue.get_nowait()
       print(f"Got command: {command}")
