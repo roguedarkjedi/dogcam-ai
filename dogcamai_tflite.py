4c581f36d25a68a12d7ef86d8e2f95abe4166ff6
RogueDarkJedi <roguedarkjedi@users.noreply.github.com>
2021-03-27 14:31:41 -0700

    Update edgetpu code to new API

--- dogcamai_tflite.py
+++ dogcamai_tflite.py
@@ -1,6 +1,7 @@
 from dogcamlogger import DogCamLogger
 from dogcamaibase import DogCamAIBase
-from edgetpu.detection.engine import DetectionEngine
+from pycoral.adapters import common, detect
+from pycoral.utils.edgetpu import make_interpreter
 from PIL import Image
 import cv2
 
@@ -8,7 +9,8 @@ class DogCamAITFLite(DogCamAIBase):
   __net = None
 
   def __init__(self, fileLocation:str):
-    self.__net = DetectionEngine(fileLocation)
+    self.__net = make_interpreter(fileLocation)
+    self.__net.allocate_tensors()
     super().__init__()
 
   def _ProcessImageInternal(self):
@@ -16,19 +18,23 @@ class DogCamAITFLite(DogCamAIBase):
     img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     img = Image.fromarray(img)
 
+    # Prepare image data
+    _, scale = common.set_resized_input(self.__net, img.size, lambda size : img.resize(size, Image.ANTIALIAS))
+
+    # Invoke the model
+    self.__net.invoke()
+
     # Run the tensorflow model
-    detectionData = self.__net.detect_with_image(img, self._minConfidence, top_k=1,
-      keep_aspect_ratio=True, relative_coord=False)
+    detectionData = detect.get_objects(self.__net, self._minConfidence, scale)
 
     for obj in detectionData:
-      if (not self._targetID or (isinstance(self._targetID, list) and obj.label_id in self._targetID)):
-        self._LogObjectFound(obj.label_id, obj.score)
+      if (not self._targetID or (isinstance(self._targetID, list) and obj.id in self._targetID)):
+        self._LogObjectFound(obj.id, obj.score)
 
         # Get the bounding box of the object
-        box = obj.bounding_box.flatten().astype(int)
-        (left, top, right, bottom) = box
+        box = obj.bbox
 
-        self._HandleObjectDetectionResult(left, right, top, bottom)
+        self._HandleObjectDetectionResult(box.xmin, box.xmax, box.ymin, box.ymax)
 
         # If we found atleast one object, then we can exit out.
         break
